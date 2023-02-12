623deea7d198bf920274c4b1287047560f560cb3
AlejandraDominguezCereijo <adominguezcereijo@danielcastelao.org>
2023-02-12 16:56:15 +0100

    calculadora

--- src/EntradaSalida.java
+++ src/EntradaSalida.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 /**
  * Libreria para sacar por dispositivos un mensaje y
  * pedir por consola distintos tipos de datos
- * @version 0.1
+ *
  * @author Damian Nogueiras
+ * @version 0.1
  */
 public class EntradaSalida {
 
@@ -17,13 +18,21 @@ public class EntradaSalida {
      * opción de uso de utilizar una ventana en el escritorio para sacar un mensaje
      */
     public static final int SALIDA_WINDOW = 2;
+    public static final int SUMA = 1;
+    public static final int RESTA = 2;
+    public static final int MULTIPLICACION = 3;
+    public static final int DIVISION = 4;
 
 
-    private void EntradaSalida(){};
+    private void EntradaSalida() {
+    }
+
+    ;
 
     /**
      * Salida por ventana o consala de un mensaje
-     * @param msj cadena que queremos imprimir
+     *
+     * @param msj    cadena que queremos imprimir
      * @param device dispositivo de salida<br>consola: SALIDA_CONSOLA<br>ventana:SALIDA_VENTANA
      * @return si es true: correcto si no false
      */
@@ -39,9 +48,9 @@ public class EntradaSalida {
 
             case SALIDA_WINDOW:
                 try {
-                    JOptionPane.showMessageDialog(null,"Sale por un ventana " + msj);
+                    JOptionPane.showMessageDialog(null, "Sale por un ventana " + msj);
                     return true;
-                } catch(Exception e) {
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                     return false;
                 }
@@ -52,21 +61,73 @@ public class EntradaSalida {
 
     /**
      * Método para obtener cadena de caracteres por consola
+     *
      * @param comentario
      * @return comentario: nos va  a retornar el String que introdujo el usuario previamente
      */
-    public static String entrada(String comentario){
+    public static String entrada(String comentario) {
 
         return comentario;
     }
+
     /**
      * Método para obtener datos enteros por consola
+     *
      * @param coment
      * @return coment: nos va  a retornar el int que introdujo el usuario previamente
      */
-    public static int entrada (int coment){
+    public static int entrada(int coment) {
 
 
         return coment;
+    }
+    /**
+     * Método para realizar operaciones, sumar, restar multiplicar y dividir
+     *
+     * @param dato1,dato2 son los valoros con los que realizar la operacion
+     * @param operacion es para indicar la operacion que queremos realizar
+     * @return operacion
+     */
+    public static int calculadora(float dato1, float dato2, int operacion) {
+            switch (operacion) {
+                case SUMA:
+                    try {
+                        float suma = dato1 + dato2;
+                        System.out.println("El resultado de la suma es: " + suma);
+                    } catch (Exception calcular) {
+                        System.out.println("Error al realizar la operacion");
+                        System.out.println(calcular.getMessage());
+                    }
+                    break;
+                case RESTA:
+                    try {
+                        float resta = dato1 - dato2;
+                        System.out.println("El resultado de la resta es: " + resta);
+                    } catch (Exception calcular) {
+                        System.out.println("Error al realizar la operacion");
+                        System.out.println(calcular.getMessage());
+                    }
+                    break;
+                case MULTIPLICACION:
+                    try {
+                        float multiplicacion = dato1 * dato2;
+                        System.out.println("El resultado de la multiplicacion es: " + multiplicacion);
+                    } catch (Exception calcular) {
+                        System.out.println("Error al realizar la operacion");
+                        System.out.println(calcular.getMessage());
+                    }
+                    break;
+                case DIVISION:
+                    try {
+                        float division =(Integer) Math.round(dato1/dato2);/*con Math.round conseguimos que solo nos devuelva la parte entera de la division*/
+                        System.out.println("El resultado de la division es: " + division);
+                    } catch (Exception calcular) {
+                        System.out.println("Error al realizar la operacion");
+                        System.out.println(calcular.getMessage());
+                    }
+                    break;
+            }
+            return operacion;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         boolean isOK_ventana = false;// aqui estan declarados como false solamente para inicializarlo podria estar true sin problema
         boolean isOK_consola = false;
+        /*boolean isOK_suma = false;
+        boolean isOK_resta = false;
+        boolean isOK_multiplicacion = false;
+        boolean isOK_division = false;*/
 
         isOK_consola = EntradaSalida.salida("Hola", EntradaSalida.SALIDA_CONSOLA);
         if (!isOK_consola) System.out.println("ERROR CONSOLA");
         isOK_ventana = EntradaSalida.salida("Hello world", EntradaSalida.SALIDA_WINDOW);
         if (!isOK_ventana) System.out.println("ERROR VENTANA");
 
-
         Scanner cubrir = new Scanner(System.in);
         System.out.println("Escribe un comentario: ");
         try {
-            String comentario =cubrir.nextLine();
+            String comentario = cubrir.nextLine();
             System.out.println(EntradaSalida.entrada(comentario));
-        }catch (Exception cadena){
+        } catch (Exception cadena) {
             System.out.println("Error al añadir el comentario");
             System.out.println(cadena.getMessage());
         }
 
         System.out.println("Dime tu edad: ");
         try {
-            int coment= cubrir.nextInt();
+            int coment = cubrir.nextInt();
             System.out.println(EntradaSalida.entrada(coment));
-        }catch (Exception entero){
+        } catch (Exception entero) {
             System.out.println("Error al añadir la edad");
             System.out.println(entero.getMessage());
         }
+        System.out.println("¿Que operacion quieres realizar? \n1 Suma \n2 Resta \n3 Multiplicacion \n4 Division");
+        int operacion = cubrir.nextInt();
+        EntradaSalida.calculadora(5, 2, operacion);
 
-
-
+        /*isOK_suma=EntradaSalida.calculadora(10,5,EntradaSalida.SUMA);
+        isOK_resta=EntradaSalida.calculadora(10,5,EntradaSalida.RESTA);
+        isOK_multiplicacion=EntradaSalida.calculadora(3,5,EntradaSalida.MULTIPLICACION);
+        isOK_division=EntradaSalida.calculadora(7,2,EntradaSalida.DIVISION);*/
     }
 }
